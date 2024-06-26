bfdde4d57dad818a8410af2732ed31695749e237
M4THEUSDev <assisma2002@gmail.com>
2024-06-25 21:51:46 -0300

    deploy completo

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,13 @@ import { styled } from "styled-components"
 import EstilosGlobais from "./componentes/EstilosGlobais"
 import Cabecalho from "./componentes/Cabecalho"
 import BarraLateral from "./componentes/BarraLateral"
+import Banner from "./componentes/Banner"
+import bannerBackground from './assets/banner.png'
+import Galeria from "./componentes/Galeria"
+
+import fotos from './fotos.json'
+import { useState } from "react"
+import ModalZoom from "./componentes/ModalZoom"
 
 
 // componente styled 
@@ -11,13 +18,68 @@ const FundoGradiente = styled.div`
   min-height: 100vh; // QUERO NO MINÍMO 100% do que tem disponivel na tela
 `
 
-function App() { 
+const AppContainer = styled.div`
+  width: 1440px;
+  margin: 0 auto;
+  max-width: 100%;
+`
+
+const MainContainer = styled.main`
+  display: flex; 
+  gap: 24px;
+`
+
+const ConteudoGaleria = styled.section`
+  display: flex;
+  flex-direction: column;
+  flex-grow: 1;
+`
+
+
+
+const App = () => {
+  const [fotosDaGaleria, setFotosDaGaleria] = useState(fotos)
+  const [fotoSelecionada, setFotoSelecionada] = useState(null)
+
+  const aoAlternarFavorito = (foto) => {
+    if (foto.id === fotoSelecionada?.id) {
+        setFotoSelecionada({
+          ...fotoSelecionada,
+          favorita: !fotoSelecionada.favorita
+      })
+    }
+    setFotosDaGaleria(fotosDaGaleria.map(fotoDaGaleria => {
+      return {
+        ...fotoDaGaleria,
+        favorita: fotoDaGaleria.id === foto.id ? !foto.favorita : fotoDaGaleria.favorita
+      }
+    }))
+  }
 
   return (
     <FundoGradiente>
-       <EstilosGlobais />
-       <Cabecalho />
-       <BarraLateral />
+      <EstilosGlobais />
+      <AppContainer>
+        <Cabecalho />
+        <MainContainer>
+          <BarraLateral />
+          <ConteudoGaleria>
+          <Banner
+            texto="A galeria mais completa de fotos do espaço!"
+            backgroundImage={bannerBackground}
+          />
+          <Galeria 
+            aoFotoSelecionada={foto => setFotoSelecionada(foto)} 
+            aoAlternarFavorito={aoAlternarFavorito}
+            fotos={fotosDaGaleria}
+          />
+          </ConteudoGaleria>
+        </MainContainer>
+      </AppContainer>
+      <ModalZoom foto={fotoSelecionada}
+      aoFechar={() => setFotoSelecionada(null)}
+      aoAlternarFavorito={aoAlternarFavorito}
+      />
     </FundoGradiente>
   )
 }
